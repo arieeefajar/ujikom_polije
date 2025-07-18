666f6d1209579df7dd511a0d763e7796947d6f87
arieeefajar <ariefajar739@gmail.com>
2025-07-18 19:47:59 +0700

    feat(home): create home page

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,15 +8,25 @@ class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+    return Obx(
+      () => Scaffold(
+        body: controller.pages[controller.curretIndex.value],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: controller.curretIndex.value,
+          onTap: controller.changePage,
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.blue,
+          showUnselectedLabels: true,
+          showSelectedLabels: true,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.access_time),
+              label: 'Presensi',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
         ),
       ),
     );
